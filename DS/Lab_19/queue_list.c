e85c987f0a56f7bb645a2ac650bf6f992a18293e
Ajay Sankaliya <120821791+AjaySankaliya@users.noreply.github.com>
2025-02-21 11:30:43 +0530

    Add files via upload

--- DS/Lab_19/queue_list.c
+++ DS/Lab_19/queue_list.c
@@ -51,6 +51,38 @@ void dequeue()
 	}
 }
 
+void middle()
+{
+	struct node *temp;
+	temp=first;
+	if(first==NULL)
+	{
+		printf("stack is underflow\n");
+	}
+	else
+	{
+		int count=0;
+		while(temp!=NULL)
+		{
+			temp=temp->next;
+			count++;
+		}
+		printf("count: %d\n",count);
+		int middle=(count-1)/2;
+		printf("middle num: %d\n",middle);
+		
+		temp=first;
+		
+		int i;
+		for(i=0;i<middle;i++)
+		{
+			temp=temp->next;
+		}
+		
+		printf("%d\n",temp->data);
+	}
+}
+
 void display()
 {
 	struct node *temp;
@@ -72,11 +104,12 @@ void display()
 void main()
 {
 	int n,n1;
-	while(n!=4)
+	while(n!=5)
 	{
 	printf("1.Insert last\n");
 	printf("2.Delete first\n");
 	printf("3.Display\n");
+	printf("4.Middle\n");
 	scanf("%d",&n);	
 	switch(n)
 	{
@@ -90,6 +123,9 @@ void main()
 			break;
 		case 3:
 			display();
+			break;
+		case 4:
+			middle();
 			break;
 		default:
 			printf("case have not found\n");
